0ee2a1b01877097a3c333a87eca354515889a0d0
Dmitry-dms <dmitry.cbip@gmail.com>
2021-10-21 17:11:34 +0500

    second commit

---
+++ src/components/PostForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- убираем стандартное действие браузера по умолчанию-->
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <my-input
+      v-model:value="post.title"
+      v-focus
+      type="text"
+      placeholder="Название"
+    />
+    <my-input v-model:value="post.body" type="text" placeholder="Описание" />
+    <my-button
+      @click="createPost"
+      style="align-self: flex-end; margin-top: 15px"
+      >Add</my-button
+    >
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      // генерируем событие для изменения массива постов
+      this.$emit("create", this.post);
+      this.post = {};
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: center;
+}
+</style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,23 @@
+<template>
+ <button class="btn">
+   <!-- все, что помещаем внутрь my-button уходит в slot -->
+<slot>
+
+</slot>
+ </button>
+</template>
+
+<script>
+export default {
+ name: 'my-button'
+}
+</script>
+
+<style scoped>
+.btn {
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
